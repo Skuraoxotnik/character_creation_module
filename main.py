66918b3525c2abc1cfe2d48668373fb9a014e88b
Виктор Федотов <skuraoxotnik@gmail.com>
2023-09-17 20:31:13 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -87,6 +88,19 @@ def main():
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class = choice_char_class()
+    print(start_training(char_name, char_class))
+
+
+def main() -> None:
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
 
